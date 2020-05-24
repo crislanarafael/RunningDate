102f26614e33be8140fae9af285f88388edb0757
Crislana Rafael <crislanarafael@gmail.com>
2020-05-24 10:59:33 -0700

    Update config.php

--- practice/config.php
+++ practice/config.php
@@ -1,13 +1,16 @@
 <?php
+
+/* Project utilized some Tutorial Republic source code to understand how to implment a login/registration system
+Link: https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php */
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'itsaulgoodman');
-define('DB_NAME', 'practice_database');
+define('server', 'localhost');
+define('username', 'root');
+define('password', 'itsaulgoodman');
+define('name', 'practice_database');
 
 /* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$link = mysqli_connect(server, username, password, name);
 
 // Check connection
 if($link === false){
